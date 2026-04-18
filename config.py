9cbbd7b5aa89b2fac72435c3786ecdb6322e5775
loveppb12-cmyk <loveppb12@gmail.com>
2026-04-18 21:50:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = "8704841733:AAHPN_TEC1XzbQPbhzZ9QvJa9xPBsNShaJc"
 SESSION_STRING = "1BVtsOKEBuxo2gXzqGpnwlF8LpSFxw8tkzKtAWW4kenYaBF9OQEaRQoWe9ti5qRje1tSxH8c_cwoeBHaUUwysg3cdwBXO-WHuJgnywdRL5R1A-sgw4MuVlkiNIiJmZcvCsRVgOiavZyapKzJkutRmwn5qs7kC0pK39_151Xow17aB8Bd0h0r5hBPOBJspDLMPyDUYhpJdTv6hALUfSyfhhEg3YbZXtY7GUahFxce2CDbRZzituNe5Mso3108C7Z9KiWxoE9yiy1kw5Gp1rA29TlIXF3DyoMTt1JRHlfZFs6MyX90udNehHKbIsRQoVmUwBYI7lWxEenkMblh-n7FZ_TDh3gFp-AQ="
 
 # Deletion delay in seconds
-DELETE_DELAY = 5
+DELETE_DELAY = 120
 
 # Heroku configuration
 PORT = int(os.environ.get('PORT', 8080))
